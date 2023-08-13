56a0cc7db74e2e9dcddf2b144bbedd166670c086
Bob Witmer <bwitmer@golden.net>
2023-08-13 10:50:58 -0400

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Bob Witmer on 2023-08-13.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
